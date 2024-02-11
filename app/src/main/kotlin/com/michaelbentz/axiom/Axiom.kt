7b4c4fb3948397fe8b1dc2311ae8b3fdc66bc680
Michael Bentz <michael@strigate.com>
2024-02-11 20:55:09 +0200

    Add @HiltAndroidApp, @AndroidEntryPoint

--- app/src/main/kotlin/com/michaelbentz/axiom/Axiom.kt
+++ app/src/main/kotlin/com/michaelbentz/axiom/Axiom.kt
@@ -1,7 +1,9 @@
 package com.michaelbentz.axiom
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class Axiom : Application() {
 
     override fun onCreate() {
